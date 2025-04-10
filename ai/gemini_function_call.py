fc87092f46f1424ff02d282436238d92acfc8f97
Aubrey Han <aubreyhan@gmail.com>
2025-04-10 11:46:38 +0800

    Your branch is up to date with 'origin/main'.

--- ai/gemini_function_call.py
+++ ai/gemini_function_call.py
@@ -1,14 +1,15 @@
 from google import genai
+from google.genai import types
 from google.genai.types import (
     FunctionDeclaration,
     GenerateContentConfig,
-    HttpOptions,
     Tool,
 )
 
 client = genai.Client(vertexai=True, location="us-central1", project='ai-demo-440003')
 model_id = "gemini-2.0-flash-001"
 
+#创建FunctionDeclaration
 get_sum = FunctionDeclaration(
     name="get_sum",
     description="Calculate the sum of two numbers.",
@@ -28,15 +29,42 @@ def get_sum(a: float, b: float) -> float:
     """Calculate the sum of two numbers."""
     return a + b    
 
-response = client.models.generate_content(
-    model=model_id,
-    contents='calculate the sum of 1 and 2',
-    config=GenerateContentConfig(
+contents = [
+    types.Content(
+        role="user", parts=[types.Part(text="what is the sum of 2 and 3?")]
+    )
+]
+
+config=GenerateContentConfig(
         tools=[sum_tool],
         temperature=0
-        ),
+        )
+
+response = client.models.generate_content(
+    model=model_id,
+    contents=contents,
+    config=config,
     )
 
 print(response.candidates[0].content.parts[0].function_call.args)
 tool_call = response.candidates[0].content.parts[0].function_call
-print(get_sum(**tool_call.args))
+result = get_sum(**tool_call.args)
+
+function_response_part = types.Part.from_function_response(
+    name=tool_call.name,
+    response={'response': result}
+)
+
+contents.append(types.Content(role="model", parts=[types.Part(function_call=tool_call)]))
+contents.append(types.Content(role="user", parts=[function_response_part]))
+
+final_response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    config=GenerateContentConfig(
+        tools=[sum_tool],
+        temperature=0
+        ),
+    contents=contents,
+)
+
+print(final_response.text)
