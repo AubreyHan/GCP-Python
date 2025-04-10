fda374ecc9b42829ab13fc0b39d5fafd7a8614a6
Aubrey Han <aubreyhan@gmail.com>
2025-04-10 12:46:35 +0800

    modified:   ai/demo01.py

--- ai/gemini_function_call.py
+++ ai/gemini_function_call.py
@@ -9,7 +9,6 @@ from google.genai.types import (
 client = genai.Client(vertexai=True, location="us-central1", project='ai-demo-440003')
 model_id = "gemini-2.0-flash-001"
 
-#创建FunctionDeclaration
 get_sum = FunctionDeclaration(
     name="get_sum",
     description="Calculate the sum of two numbers.",
@@ -23,11 +22,11 @@ get_sum = FunctionDeclaration(
     },
 )
 
-sum_tool = Tool(function_declarations=[get_sum])
-
-def get_sum(a: float, b: float) -> float:
+def get_sum_function(a: float, b: float) -> float:
     """Calculate the sum of two numbers."""
-    return a + b    
+    return a + b
+
+sum_tool = Tool(function_declarations=[get_sum])
 
 contents = [
     types.Content(
@@ -46,25 +45,27 @@ response = client.models.generate_content(
     config=config,
     )
 
-print(response.candidates[0].content.parts[0].function_call.args)
-tool_call = response.candidates[0].content.parts[0].function_call
-result = get_sum(**tool_call.args)
+#print(response.candidates[0].content.parts[0].function_call)
 
-function_response_part = types.Part.from_function_response(
+if response.candidates[0].content.parts[0].function_call:
+    tool_call = response.candidates[0].content.parts[0].function_call
+    result = get_sum_function(**tool_call.args)
+    #print(result)
+
+    function_response_part = types.Part.from_function_response(
     name=tool_call.name,
     response={'response': result}
-)
+    )
 
-contents.append(types.Content(role="model", parts=[types.Part(function_call=tool_call)]))
-contents.append(types.Content(role="user", parts=[function_response_part]))
+    contents.append(types.Content(role="model", parts=[types.Part(function_call=tool_call)]))
+    contents.append(types.Content(role="user", parts=[function_response_part]))
 
-final_response = client.models.generate_content(
+    final_response = client.models.generate_content(
     model="gemini-2.0-flash",
-    config=GenerateContentConfig(
-        tools=[sum_tool],
-        temperature=0
-        ),
+    config=config,
     contents=contents,
-)
+    )
 
-print(final_response.text)
+    print(final_response.text)
+
+
