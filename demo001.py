148fc1fda58e1392cc0d1401b4997bcac439d9f6
Aubrey Han <aubreyhan@live.com>
2025-04-14 20:00:22 +0800

    modified:   demo001.py

--- demo001.py
+++ demo001.py
@@ -1,18 +1,12 @@
 from google import genai
-from google.genai import types
-
-client = genai.Client(vertexai=True, location="us-central1", project='ai-demo-440003')
-
-prompt = 'explain the difference between a and b'
-model = 'gemini-2.0-flash-thinking-exp-01-21'
-
-config = types.GenerateContentConfig(
-    thinking_config=types.ThinkingConfig(thinking_budget=0),
+from google.genai import (types,  
+from google.genai.types import (
+    FunctionDeclaration,
+    GenerateContentConfig,
+    Tool,
 )
 
-response = client.models.generate_content(
-    model=model,
-    contents=prompt
+    )
 )
 
-print(response)
+print(response.text)
