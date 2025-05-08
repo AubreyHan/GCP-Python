5eeb8d201467192553d5fbb6c4fe4fab3c78cd02
Aubrey Han <aubreyhan@gmail.com>
2025-05-08 09:48:19 +0800

    modified:   gemini_thinking.py

--- gemini_thinking.py
+++ gemini_thinking.py
@@ -4,7 +4,10 @@ from google.genai import types
 client = genai.Client(vertexai=True, location="us-central1", project='ai-demo-440003')
 
 config = types.GenerateContentConfig(
-    thinking_config=types.ThinkingConfig(thinking_budget=0), 
+    thinking_config=types.ThinkingConfig(
+        thinking_budget=100,    #控制Thinking的token数(1-24576，1024以下会统一为1024，0表示关闭)
+        include_thoughts=True,  #输出是否包含思考过程的汇总
+    ), 
 )
 
 response = client.models.generate_content(
@@ -13,5 +16,6 @@ response = client.models.generate_content(
     config=config
 )
 
-print(response.usage_metadata.thoughts_token_count)
+print(response.text)
+print(response.usage_metadata.thoughts_token_count) #所使用的thinking token数
 
