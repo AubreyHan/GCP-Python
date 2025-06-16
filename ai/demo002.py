2fdcde6e48d3bf6fe2f0043349bea620e7f9b277
Aubrey Han <aubreyhan@gmail.com>
2025-06-16 11:03:00 +0800

    auto-commit

--- ai/demo002.py
+++ ai/demo002.py
@@ -34,20 +34,7 @@ def generate():
     top_p = 1,
     seed = 0,
     max_output_tokens = 65535,
-    safety_settings = [types.SafetySetting(
-      category="HARM_CATEGORY_HATE_SPEECH",
-      threshold="OFF"
-    ),types.SafetySetting(
-      category="HARM_CATEGORY_DANGEROUS_CONTENT",
-      threshold="OFF"
-    ),types.SafetySetting(
-      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
-      threshold="OFF"
-    ),types.SafetySetting(
-      category="HARM_CATEGORY_HARASSMENT",
-      threshold="OFF"
-    )]
-    ),
+    )
   
 
   for chunk in client.models.generate_content_stream(
