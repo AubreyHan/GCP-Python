0b0a74f32bc232b673aa1c2f9554f63bf073cfc4
Aubrey Han <aubreyhan@gmail.com>
2025-06-19 13:50:48 +0800

    auto-commit

--- ai/demo001.py
+++ ai/demo001.py
@@ -1,6 +1,8 @@
 video_file_name = "/Users/yuanhancn/Tools/FFMpeg/5noaudio.mp4"
 video_bytes = open(video_file_name, 'rb').read()
 
+
+
 response = client.models.generate_content(
     model='models/gemini-2.5-flash-preview-05-20',
     contents=types.Content(
