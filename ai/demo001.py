71864c2b147e585150fb8f231926f3c5cef05cb7
Aubrey Han <aubreyhan@gmail.com>
2025-06-19 11:49:16 +0800

    auto-commit

---
+++ ai/demo001.py
@@ -0,0 +1,58 @@
+from google import genai
+from google.genai import types
+import base64
+
+def generate():
+  client = genai.Client(
+      vertexai=True,
+      project="ai-demo-440003",
+      location="global",
+  )
+
+  video1 = types.Part.from_uri(
+      file_uri="gs://hy-aibucket-001/5noaudio.mp4",
+      mime_type="video/mp4",
+  )
+
+  model = "gemini-2.5-flash"
+  contents = [
+    types.Content(
+      role="user",
+      parts=[
+        video1,
+        types.Part.from_text(text="""请分析以上视频""")
+      ]
+    )
+  ]
+
+  generate_content_config = types.GenerateContentConfig(
+    temperature = 1,
+    top_p = 1,
+    seed = 0,
+    max_output_tokens = 65535,
+    safety_settings = [types.SafetySetting(
+      category="HARM_CATEGORY_HATE_SPEECH",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_DANGEROUS_CONTENT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_HARASSMENT",
+      threshold="OFF"
+    )],
+    thinking_config=types.ThinkingConfig(
+      thinking_budget=-1,
+    ),
+  )
+
+  for chunk in client.models.generate_content_stream(
+    model = model,
+    contents = contents,
+    config = generate_content_config,
+    ):
+    print(chunk.text, end="")
+
+generate()
