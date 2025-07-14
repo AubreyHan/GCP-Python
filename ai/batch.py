20db736a2122e8cc136fc5a790cc84094cc8eece
Aubrey Han <aubreyhan@gmail.com>
2025-07-14 09:14:38 +0800

    auto-commit

--- ai/batch.py
+++ ai/batch.py
@@ -14,7 +14,7 @@ job = client.batches.create(
     model="gemini-2.5-flash",
     # Source link: https://storage.cloud.google.com/cloud-samples-data/batch/prompt_for_batch_gemini_predict.jsonl
     src="gs://hy-batch-001/demo001.jsonl",
-    config=CreateBatchJobConfig(dest=output_uri),
+    config=CreateBatchJobConfig(dest=''),
 )
 print(f"Job name: {job.name}")
 print(f"Job state: {job.state}")
