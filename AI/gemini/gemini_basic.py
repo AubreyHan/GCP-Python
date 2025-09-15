ae88045c615f925b6e7d4e57117ad23bc8a310d5
Aubrey Han <aubreyhan@gmail.com>
2025-09-15 10:32:36 +0800

    Changes to be committed:

--- AI/gemini/gemini_basic.py
+++ AI/gemini/gemini_basic.py
@@ -1,53 +1,27 @@
 from google import genai
-from google.genai import types
-import base64
-import os
+from pydantic import BaseModel
 
-def generate():
-  client = genai.Client(
-      vertexai=True,
-      project='hy-ai-demo',
-      location='global'
+class Recipe(BaseModel):
+    recipe_name: str
+    ingredients: list[str]
+
+
+client = genai.Client(
+    vertexai=True,
+    project='hy-ai-demo',
+    location='global'
   )
 
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="List a few popular cookie recipes, and include the amounts of ingredients.",
+    config={
+        "response_mime_type": "application/json",
+        "response_schema": list[Recipe],
+    },
+)
+# Use the response as a JSON string.
+print(response.text)
 
-  model = "gemini-2.5-flash"
-  contents = [
-    types.Content(
-      role="user",
-      parts=[
-        types.Part.from_text(text="""hello""")
-      ]
-    )
-  ]
-
-  generate_content_config = types.GenerateContentConfig(
-    temperature = 1,
-    top_p = 1,
-    max_output_tokens = 65535,
-    safety_settings = [types.SafetySetting(
-      category="HARM_CATEGORY_HATE_SPEECH",
-      threshold="OFF"
-    ),types.SafetySetting(
-      category="HARM_CATEGORY_DANGEROUS_CONTENT",
-      threshold="OFF"
-    ),types.SafetySetting(
-      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
-      threshold="OFF"
-    ),types.SafetySetting(
-      category="HARM_CATEGORY_HARASSMENT",
-      threshold="OFF"
-    )],
-    thinking_config=types.ThinkingConfig(
-      thinking_budget=-1,
-    ),
-  )
-
-  for chunk in client.models.generate_content_stream(
-    model = model,
-    contents = contents,
-    config = generate_content_config,
-    ):
-    print(chunk, end="")
-
-generate()
+# Use instantiated objects.
+my_recipes: list[Recipe] = response.parsed
