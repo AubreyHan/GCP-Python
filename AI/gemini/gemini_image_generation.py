8aaaeae8016314695f9b4caaa4d7429f63d70888
Aubrey Han <aubreyhan@gmail.com>
2025-09-15 16:56:33 +0800

    modified:   AI/gemini/gemini_image_generation.py

--- AI/gemini/gemini_image_generation.py
+++ AI/gemini/gemini_image_generation.py
@@ -1,16 +1,50 @@
 from google import genai
+from google.genai import types
+import base64
+import os
 
-client = genai.Client(
-    project='hy-ai-demo',
-    vertexai=True,
-    location="global"
-)
+def generate():
+  client = genai.Client(
+      vertexai=True,
+      api_key='AQ.Ab8RN6IDa6Ly3J47e-Bpy4C6HwOsypBWYVscL9vffTT4w5iv_Q',
+  )
 
-prompt = (
-    "Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme"
-)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash-image-preview",
-    contents=[prompt],
-)
+  model = "gemini-2.5-flash-image-preview"
+  contents = [
+    types.Content(
+      role="user",
+      parts=[
+        types.Part.from_text(text="""请画一只小狗""")
+      ]
+    )
+  ]
+
+  generate_content_config = types.GenerateContentConfig(
+    temperature = 1,
+    top_p = 0.95,
+    max_output_tokens = 32768,
+    response_modalities = ["TEXT", "IMAGE"],
+    safety_settings = [types.SafetySetting(
+      category="HARM_CATEGORY_HATE_SPEECH",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_DANGEROUS_CONTENT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_HARASSMENT",
+      threshold="OFF"
+    )],
+  )
+
+  for chunk in client.models.generate_content_stream(
+    model = model,
+    contents = contents,
+    config = generate_content_config,
+    ):
+    print(chunk.text, end="")
+
+generate()
