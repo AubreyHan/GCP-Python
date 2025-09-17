b04230ceae5427350f7e00940630fa118ba900a9
Aubrey Han <aubreyhan@live.com>
2025-09-18 06:43:34 +0800

    Changes to be committed:

--- temp/demo001.py
+++ temp/demo001.py
@@ -1,1 +1,50 @@
-print(1e-10)
+from google import genai
+from google.genai import types
+import base64
+import os
+
+def generate():
+  client = genai.Client(
+      vertexai=True,
+      api_key='AQ.Ab8RN6LmEqsHFfmx1gZixXwWCKFyUuV-uheh5HriOXuy0dK_vA',
+  )
+
+
+  model = "gemini-2.5-flash-image-preview"
+  contents = [
+    types.Content(
+      role="user",
+      parts=[
+        types.Part.from_text(text="""hello""")
+      ]
+    )
+  ]
+
+  generate_content_config = types.GenerateContentConfig(
+    temperature = 1,
+    top_p = 0.95,
+    max_output_tokens = 32768,
+    response_modalities = ["TEXT", "IMAGE"],
+    safety_settings = [types.SafetySetting(
+      category="HARM_CATEGORY_HATE_SPEECH",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_DANGEROUS_CONTENT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_HARASSMENT",
+      threshold="OFF"
+    )],
+  )
+
+  for chunk in client.models.generate_content_stream(
+    model = model,
+    contents = contents,
+    config = generate_content_config,
+    ):
+    print(chunk.text, end="")
+
+generate()
